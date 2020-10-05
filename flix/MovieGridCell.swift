efbfa505d52cbca96c67820a4761fd22e8cbf80c
Ensar Dogrusoz <kaandogrusoz00@gmail.com>
2020-10-05 02:02:03 -0400

    added superhero tab with cells

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Kaan Dogrusoz on 10/4/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+        
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
